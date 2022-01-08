0ebb8362ab868ba936905ff25d171b823b615e15
r-extream <venera_ok@mail.ru>
2022-01-08 12:38:25 +0300

    2nd course 3rd lesson

---
+++ ordersapp/models.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.db import models
+
+from mainapp.models import Product
+
+
+class Order(models.Model):
+    STATUS_FORMING = 'FM'
+    STATUS_SENT_TO_PROCEED = 'STP'
+    STATUS_PROCEEDED = 'PRD'
+    STATUS_PAID = 'PD'
+    STATUS_CANCEL = 'CNL'
+    STATUS_DONE = 'DN'
+
+    STATUSES = (
+        (STATUS_FORMING, 'Формируется'),
+        (STATUS_SENT_TO_PROCEED, 'Отправлен в обработку'),
+        (STATUS_PROCEEDED, 'Обработан'),
+        (STATUS_PAID, 'Оплачен'),
+        (STATUS_CANCEL, 'Отменен'),
+        (STATUS_DONE, 'Завершен'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUSES, default=STATUS_FORMING, max_length=5)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_total_quantity(self):
+        _items = self.orderitems.select_related()
+        _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _total_quantity
+
+    def get_total_cost(self):
+        _items = self.orderitems.select_related()
+        _total_cost = sum(list(map(lambda x: x.quantity * x.product.price, _items)))
+        return _total_cost
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_active = False
+        self.save()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ', related_name='orderitems')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+
+    def get_product_cost(self):
+        return self.product.price * self.quantity
---
+++ ordersapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from ordersapp import views as ordersapp
+
+
+app_name = 'ordersapp'
+
+
+urlpatterns = [
+    path('', ordersapp.OrderListView.as_view(), name='list'),
+    path('create/', ordersapp.OrderCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', ordersapp.OrderUpdateView.as_view(), name='update'),
+    path('read/<int:pk>/', ordersapp.OrderDetailView.as_view(), name='read'),
+    path('delete/<int:pk>/', ordersapp.OrderDeleteView.as_view(), name='delete'),
+    path('complete/<int:pk>/', ordersapp.complete, name='complete'),
+]
