dbfbe4250f7671c3f7e44af7f47b21f748c3e13c
r-extream <venera_ok@mail.ru>
2022-01-21 09:35:55 +0300

    2nd course 5th lesson

--- mygeekshop/settings.py
+++ mygeekshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)=@vos2d&^2l=8*3h7!1q=w-m06i7fsm3=269p6#&wbci2^opi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,13 +89,20 @@ WSGI_APPLICATION = 'mygeekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
