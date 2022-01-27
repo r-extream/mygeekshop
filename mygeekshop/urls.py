1974ed37ab9229deb06026facf87f0a7b96ee35a
r-extream <venera_ok@mail.ru>
2022-01-27 20:15:15 +0300

    2nd course 6th lesson

--- mygeekshop/urls.py
+++ mygeekshop/urls.py
@@ -33,3 +33,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [path(r'^__debug__/', include(debug_toolbar.urls))]
